c96124e1a4b7f3668ad588181f7fd52c44c520d4
조유민 <nationmilo@gmail.com>
2024-12-16 14:05:01 +0900

    feat: jpa & testcode sample

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
+    
     public static void main(String[] args) {
+        Test sample = new Test();
+        sample.setData("Hello, world!");
+        String name = sample.getData();
+        System.out.println(name);
+        
         SpringApplication.run(JpashopApplication.class, args);
     }
 
