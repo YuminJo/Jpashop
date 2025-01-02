4529cf573d2d70ed6d8c3339edb710688c33fca6
조유민 <nationmilo@gmail.com>
2025-01-02 20:45:22 +0900

    feat: Item Logic

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
+//Setter를 사용하지 않는 것이 좋다. Setter를 사용하면 변경 포인트가 많아져서 추적이 어려워진다.
 @Getter @Setter
 public abstract class Item {
     @Id
@@ -24,4 +26,23 @@ public abstract class Item {
     
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+    
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
